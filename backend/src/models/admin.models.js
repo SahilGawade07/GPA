ccbeb2b3031bbf9880e0eba1d92ebb9fedf21ef3
Sahil1459 <sahilgawade712@gmail.com>
2025-02-20 19:23:01 +0530

    add model for admin and instituteProfile

---
+++ backend/src/models/admin.models.js
@@ -0,0 +1,77 @@
+import mongoose from "mongoose";
+
+const adminSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    middleName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["male", "female", "other"],
+      required: true,
+    },
+    post: {
+      type: String,
+      enum: ["clerk", "principal"],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hashSync(this.password, 10);
+    next(); 
+})
+
+userSchema.methods.isPasswordcorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+    },
+process.env.ACCESS_TOKEN_SECRET,
+{
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+})
+}
+
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign({
+        _id: this._id,
+    },
+process.env.REFRESH_TOKEN_SECRET,
+{
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+})
+}
+
+export const Admin = mongoose.model("Admin", adminSchema);
---
+++ backend/src/models/collegeProfile.models.js
@@ -0,0 +1,54 @@
+import mongoose from "mongoose";    
+
+const collegeProfileeSchema = new mongoose.Schema({
+    collegeName: {
+        type: String,
+        default: 'Government Polytechnic Awsari',
+        required: true
+    },
+    aicteCode: {
+        type: String,
+        default: '0',
+        required: true
+    },
+    aicteApprovalLetter: {
+        type: String,
+        required: true
+    },
+    msbteCode: {
+        type: String,
+        default: '0',
+        required: true
+    },
+    dteCode: {
+        type: String,
+        default: '0',
+        required: true
+    },
+    numberOfBracnhes: {
+        type: Number,
+        default: 0,
+        required: true
+    },
+    branchCode: {
+        type: String,
+        default: '0',
+        required: true
+    },
+    branchIntake: {
+        type: String,
+        required: true
+    },
+    instituteVision: {
+        type: String,
+        required: true
+    },
+    instituteMission: {
+        type: String,
+        required: true
+    },
+
+
+},{timestamps: true});
+
+export const CollegeProfile = mongoose.model('CollegeProfile', collegeProfileeSchema);
